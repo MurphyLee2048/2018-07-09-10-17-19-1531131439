2b9daa602858635b9d13cae7f8806919c5dfa7d9
MurphyLee <36724748+MurphyLee2048@users.noreply.github.com>
2019-07-12 06:31:45 +0800

    commit practice 06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,24 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    //>My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.
+    private int klass;
+
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. " + "I teach Class " + klass;
+    }
 }
