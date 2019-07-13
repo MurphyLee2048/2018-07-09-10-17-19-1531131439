6334e9b6c78f97ad9bc1be3ba181eb1ae1333138
MurphyLee <36724748+MurphyLee2048@users.noreply.github.com>
2019-07-13 12:31:12 +0800

    feat: complete practice 10, used java 8 stream.

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,47 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> students = new ArrayList<Student>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (!students.contains(student)){
+            System.out.print("It is not one of us.\n");
+        }else {
+            leader = student;
+        }
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+        student.setKlass(this);
+    }
+
+    public boolean isIn(Student student) {
+        if (students.contains(student)) {
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,27 @@
 package practice10;
 
-public class Student {
+public class Student extends Person{
+    public Student(int personID, String name, int age, Klass klass) {
+        super(personID, name, age);
+        this.klass = klass;
+    }
+
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass.getLeader() != null && klass.getLeader().getName() == super.getName()) {
+            return super.introduce() + " I am a Student. I am Leader of " + klass.getDisplayName() + ".";
+        } else return super.introduce() + " I am a Student. I am at " + klass.getDisplayName() + ".";
+    }
+
+
 }
